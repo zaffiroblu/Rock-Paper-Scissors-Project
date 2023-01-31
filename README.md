b2f56b38cfb0a612ba82abefe0e3fee29e6f7cb1
zaffiroblu <jenshivel@gmail.com>
2023-01-31 11:24:36 +0100

    Add a story about the roommate to make the app more engaging.

--- README.md
+++ README.md
@@ -6,12 +6,16 @@
 
 "Paper" icon by Freepik on Flaticon: https://www.flaticon.com/premium-icon/new-document_2377960?term=paper&page=1&position=4&page=1&position=4&related_id=2377960&origin=search
 
-"Hand Paper" icon by Pixel perfect on Flaticon: https://www.flaticon.com/free-icon/open-hands_3179232?related_id=3179232&origin=search
+"Hand Paper" icon by Cuputo on Flaticon: https://www.flaticon.com/premium-icon/open-hand_3562327?term=hand%20open&page=1&position=58&page=1&position=58&related_id=3562327&origin=search
 
 "Scissors" icon by Freepik on Flaticon: https://www.flaticon.com/premium-icon/scissors_2542041?term=scissors&page=1&position=3&page=1&position=3&related_id=2542041&origin=search
 
-"Hand Scissors" icon by Alfian Dwi Hartanto on Flaticon: https://www.flaticon.com/premium-icon/two_4958040?related_id=4958040&origin=search
+"Hand Scissors" icon by Alfian Dwi Hartanto on Flaticon: https://www.flaticon.com/search?word=scissors%20hand&order_by=4
 
 "Loser" icon by surang on Flaticon: https://www.flaticon.com/free-icon/loser_5849293?term=loser&page=1&position=14&page=1&position=14&related_id=5849293&origin=search
 
 "Trophy" icon by Freepik on Flaticon: https://www.flaticon.com/free-icon/trophy_1021151?term=winner&related_id=1021151
+
+"Ice Cream" Popsicle icons created by bqlqn on Flaticon: https://www.flaticon.com/free-icon/ice-cream-stick_4363705?term=ice+cream&related_id=4363705
+
+"Thumbs Down" icon created by Cuputo on Flaticon: https://www.flaticon.com/free-icon/thumb-down_3562269?related_id=3562269&origin=pack
--- rps.js
+++ rps.js
@@ -1,124 +1,232 @@
-// Here are some vars to keep track of the points for the human and for the computer.
+// First I grab the relevant elements from HTML and save them as variables in JS:
+const headerTextIntro = document.getElementById('header-text-intro');
+const iceCream = document.getElementById('ice-cream');
+const rockBtn = document.getElementById('id-rock');
+const paperBtn = document.getElementById('id-paper');
+const scissorsBtn = document.getElementById('id-scissors');
+const showMoves = document.getElementById('show-moves');
+const humMoveImg = document.getElementById('hum-move-img');
+const humEndImg = document.getElementById('hum-end-img');
+const showMovesText = document.getElementById('moves-text');
+const comMoveImg = document.getElementById('com-move-img');
+const comEndImg = document.getElementById('com-end-img');
+const winnerRow = document.getElementById('winner-row');
+const winnerText = document.getElementById('winner-text');
+const scoreHum = document.getElementById('score-hum');
+const scoreCom = document.getElementById('score-com');
+const gameWinnerText = document.getElementById('game-winner-text');
+const gameEndBtn = document.getElementById('game-end-btn');
+
+// Here are some variables to keep track of the points for the human (hum)
+//and for the computer/roommate (com).
 let humPoints = 0;
 let comPoints = 0;
 
-// This defines the available choices for both players.
+// This array defines the available choices for both players.
 const gameArray = ['rock', 'paper', 'scissors'];
 
-//Alright, now it's time for a fancy UI!
-// First I need to grab the buttons and the container from the HTML and save them as variables in JS:
-const rockBtn = document.getElementById('rocky');
-const paperBtn = document.getElementById('papery');
-const scissorsBtn = document.getElementById('scissory');
-const body = document.querySelector('body');
-const feedbackContainer = document.getElementById('feedback-row');
-const humMoveImg = document.getElementById('hum-move-img');
-const feedbackPara = document.getElementById('feedback-para');
-const comMoveImg = document.getElementById('com-move-img');
-const feedbackScore = document.getElementById('feedback-score');
-const gameEndRow = document.getElementById('game-end-row');
-const gameEndImg = document.getElementById('game-end-img');
-const gameEndPara = document.getElementById('game-end-para');
-
-//And define humIndex.
+//I also go ahead and define humIndex, which will hold the index value
+// of the human move in the game.
 let humIndex;
 
+// This is a function that will be called in order to add/remove the animation classes
+// for boththe hum and com on each click.
+function animateMoves(imageClass, direction) {
+	imageClass.classList.add(direction);
+	imageClass.addEventListener('animationend', () => {
+		imageClass.classList.remove(direction);
+	});
+}
+
 // This will generate a random computer choice once called in one of the click events below
-//(This produced a number value of 0, 1 or 2, which corresponds to the length of the gameArray.)
+// It produces a number value of 0, 1 or 2, which corresponds to the length of the gameArray above.
 function comResult() {
 	let randNum = Math.floor(Math.random() * gameArray.length);
 	return randNum;
 }
 
-//These are the event listeners for my buttons:
+//These are the event listeners for the three game buttons:
 rockBtn.addEventListener('click', () => {
+	showMovesText.textContent = '';
+	winnerText.textContent = '';
+	winnerText.classList.remove('fade-in-text');
 	humIndex = gameArray.indexOf('rock');
-	humMoveImg.src = 'images/fist.png';
+	animateMoves(humMoveImg, 'animate-from-left');
+	humMoveImg.src = 'images/rock-fist-hum.png';
 	playRound();
 });
 
 paperBtn.addEventListener('click', () => {
+	showMovesText.textContent = '';
+	winnerText.textContent = '';
+	winnerText.classList.remove('fade-in-text');
 	humIndex = gameArray.indexOf('paper');
-	humMoveImg.src = 'images/open-hands.png';
+	animateMoves(humMoveImg, 'animate-from-left');
+	humMoveImg.src = 'images/paper-hand-hum.png';
 	playRound();
 });
 
 scissorsBtn.addEventListener('click', () => {
+	showMovesText.textContent = '';
+	winnerText.textContent = '';
+	winnerText.classList.remove('fade-in-text');
 	humIndex = gameArray.indexOf('scissors');
-	humMoveImg.src = 'images/scissors-hand.png';
+	animateMoves(humMoveImg, 'animate-from-left');
+	humMoveImg.src = 'images/scissors-hand-hum.png';
 	playRound();
 });
 
-// -------------------------- Below is where the real action starts.
+// ---------- Below is where the real action starts. ----------
 
 function playRound() {
-	feedbackContainer.classList.remove('d-none');
+	showMoves.classList.remove('hidden');
 
-	//Here I define a variable for and call the comResult from above.
+	//Here I define a variable for and call the comResult function from above.
+	// This is how the computer(roommate) makes a "choice."
 	const comIndex = comResult();
 
+	// This switch statement takes the comIndex as an argument and
+	// displays the appropriate hand symbol in the DOM.
 	switch (comIndex) {
 		case 0:
-			comMoveImg.src = 'images/fist.png';
+			comMoveImg.src = 'images/rock-fist-com.png';
+			animateMoves(comMoveImg, 'animate-from-right');
 			break;
 		case 1:
-			comMoveImg.src = 'images/open-hands.png';
+			comMoveImg.src = 'images/paper-hand-com.png';
+			animateMoves(comMoveImg, 'animate-from-right');
 			break;
 		case 2:
-			comMoveImg.src = 'images/scissors-hand.png';
+			comMoveImg.src = 'images/scissors-hand-com.png';
+			animateMoves(comMoveImg, 'animate-from-right');
 			break;
 	}
 
-	//Here I generate and display info about the moves of each player.
-	feedbackPara.textContent = `You've gone with ${gameArray[humIndex]}. I've chosen ${gameArray[comIndex]}.`;
+	// Here I generate and display a text that summarizes the moves of each player.
+	comMoveImg.addEventListener('animationend', () => {
+		showMovesText.classList.add('fade-in-text');
+		showMovesText.textContent = `You've gone with ${gameArray[humIndex]}. \n Your roomy has chosen ${gameArray[comIndex]}.`;
+		showMovesText.addEventListener('animationend', () => {
+			showMovesText.classList.remove('fade-in-text');
+		});
+	});
 
-	// This is a switch function that returns who won the round.
-
+	// This is a switch function a) who won the round, and
+	// b) the current points tally.
 	let outcome = humIndex - comIndex;
 
 	switch (outcome) {
 		case 0:
-			feedbackScore.textContent = `This round it a draw. The score is ${humPoints} to ${comPoints}.`;
-			gameEndPara.textContent = "Let's give it another go.";
+			showMovesText.addEventListener('animationend', () => {
+				winnerRow.classList.remove('hidden');
+				winnerText.classList.add('fade-in-text');
+				winnerText.textContent = `This round is a draw!`;
+				scoreHum.textContent = `${humPoints}`;
+				scoreCom.textContent = `${comPoints}`;
+			});
 			break;
 		case -2:
 		case 1:
 			humPoints += 1;
-			feedbackScore.textContent = `You win this round! The score is ${humPoints} to ${comPoints}.`;
-			gameEndPara.textContent = "Let's keep going!";
+			if (humPoints >= 3 || comPoints >= 3) {
+				endOfGame();
+			} else {
+				showMovesText.addEventListener('animationend', () => {
+					winnerRow.classList.remove('hidden');
+					winnerText.classList.add('fade-in-text');
+					winnerText.textContent = `You win this round!`;
+					scoreHum.textContent = `${humPoints}`;
+					scoreCom.textContent = `${comPoints}`;
+				});
+			}
 			break;
 		case -1:
 		case 2:
 			comPoints += 1;
-			feedbackScore.textContent = `I win this round! The score is ${humPoints} to ${comPoints}.`;
-			gameEndPara.textContent = "Don't give up yet!";
+			if (humPoints >= 3 || comPoints >= 3) {
+				endOfGame();
+			} else {
+				showMovesText.addEventListener('animationend', () => {
+					winnerRow.classList.remove('hidden');
+					winnerText.classList.add('fade-in-text');
+					winnerText.textContent = `Your roommate wins this round!`;
+					scoreHum.textContent = `${humPoints}`;
+					scoreCom.textContent = `${comPoints}`;
+				});
+			}
 			break;
-		default:
-			console.log(
-				"You're wasting my computing power. PLEASE choose either rock, paper, or scissors now."
-			);
-	}
-
-	if (humPoints >= 3 || comPoints >= 3) {
-		// This calls the "endofGame" function.
-		endOfGame();
 	}
 }
 
+// This function is triggered when either the human or the computer(roommate)
+// get 3 points.
 function endOfGame() {
+	winnerText.style.display = 'none';
+	gameWinnerText.classList.remove('hidden');
+	winnerRow.classList.remove('hidden');
 	rockBtn.classList.add('disabled');
 	paperBtn.classList.add('disabled');
 	scissorsBtn.classList.add('disabled');
-	gameEndRow.classList.remove('d-none');
 	if (humPoints == 3) {
-		gameEndImg.src = 'images/trophy.png';
-		gameEndPara.textContent =
-			"Yes indeed, you've gotten to 3 points before I could. You win the game! Congrats.";
-		gameEndRow.style.backgroundColor = '#53FFDA';
+		gameWinnerText.classList.add('winner-text-animation');
+		gameWinnerText.textContent = `You win the ice cream!`;
+		endAnimation();
 	} else if (comPoints == 3) {
-		gameEndImg.src = 'images/loser.png';
-		gameEndPara.textContent =
-			"Well well well, it seems I've gotten to 3 points before you. You lose the game! So sorry about that.";
-		gameEndRow.style.backgroundColor = '#FF8453';
+		gameWinnerText.classList.add('winner-text-animation');
+		gameWinnerText.textContent = `Oh no, your roomy wins!`;
+		endAnimation();
 	}
 }
+
+// This last animation sequence is triggered at the end,
+// and also makes the "play again?" button appear towards the top.
+function endAnimation() {
+	animateIceCream3();
+	iceCream.addEventListener('animationend', () => {
+		iceCream.style.display = 'none';
+		humMoveImg.style.display = 'none';
+		comMoveImg.style.display = 'none';
+		humEndImg.classList.remove('hidden');
+		comEndImg.classList.remove('hidden');
+		if (humPoints == 3) {
+			humEndImg.src = 'images/hum-win-hand.png';
+			humEndImg.classList.add('fade-in-winner');
+			comEndImg.src = 'images/thumb-down-com-small-4.png';
+			comEndImg.classList.add('fade-in-loser');
+			headerTextIntro.style.display = 'none';
+			gameEndBtn.classList.remove('d-none');
+		} else if (comPoints == 3) {
+			comEndImg.src = 'images/com-win-hand.png';
+			comEndImg.classList.add('fade-in-winner');
+			humEndImg.src = 'images/thumb-down-hum-small-4.png';
+			humEndImg.classList.add('fade-in-loser');
+			headerTextIntro.style.display = 'none';
+			gameEndBtn.classList.remove('d-none');
+		}
+	});
+}
+
+/******************* Start sequence animations ***********************/
+// Towards the bottom, because they can be!
+
+function animateIceCream() {
+	iceCream.src = 'images/ice-cream-stick.png';
+	iceCream.classList.add('animate-ice-cream');
+}
+
+function animateIceCream2() {
+	iceCream.classList.add('animate-ice-cream-shake');
+}
+
+function animateIceCream3() {
+	iceCream.classList.add('animate-ice-cream-zoom');
+}
+
+function animateIntroText() {
+	headerTextIntro.classList.remove('hidden');
+	headerTextIntro.classList.add('slow-fade-in');
+}
+
+setTimeout(animateIceCream, 500);
+setTimeout(animateIntroText, 1500);
+setTimeout(animateIceCream2, 2500);
